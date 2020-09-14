71f944ab3b21db0504c5d08258dc4c09514475f4
Mark-Kohrman <mark.kohrman@gmail.com>
2020-09-13 21:09:32 -0500

    adding index action for bananas

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/bananas">All Bananas</router-link>
     </div>
     <router-view/>
   </div>
---
+++ src/views/BananasIndex.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <h1>Look at all these bananas!!!</h1>
+    <div v-for="banana in bananas">
+      <h2>{{ banana.country }}</h2>
+      <p>{{ banana.company }}</p>
+      <p>{{ banana.price }}</p>
+
+    </div>
+   
+
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      bananas: [],
+    };
+  },
+  created: function () {
+    console.log("All the bananas!");
+    axios.get("/api/bananas").then((response) => {
+      this.bananas = response.data;
+    });
+  },
+  methods: {},
+};
+</script>
